976c908d058822fd852a0d367b8eb5cc0e0f66f6
qiuhengbin <qiuhengbin@codemao.cn>
2019-03-08 08:11:54 +0800

    feat: first commit

---
+++ src/main/java/cn/ebing/dog/api/DogApiApplication.java
@@ -0,0 +1,13 @@
+package cn.ebing.dog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogApiApplication.class, args);
+	}
+
+}
