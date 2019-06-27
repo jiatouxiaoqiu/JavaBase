467e5fd6bfa7171f7d8a018667cb223240883b30
qiuhengbin <qiuhengbin@codemao.cn>
2019-06-27 18:06:30 +0800

    1

---
+++ src/main/resources/db/mysql/migration/V20190627180450_24309__add_user.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `tbl_user`;
+CREATE TABLE IF NOT EXISTS `tbl_user`(
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(50) NOT NULL default '' COMMENT '名字',
+  `age` tinyint(4) NOT NULL DEFAULT 0 COMMENT '年龄',
+  `sex` tinyint(1) NOT NULL DEFAULT 0 COMMENT '性别 0:女性 1:男性',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '记录更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户表';
