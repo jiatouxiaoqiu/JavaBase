6d18b21302db4f3b3d09c755793dd05e021f2d02
hicery <qiuhengbin@codemao.cn>
2020-07-21 15:50:42 +0800

    1

--- src/main/java/cn/ebing/dog/api/bs/eps/forecastUnit/A0912Y610Bs.java
+++
@@ -1,7 +0,0 @@
-package cn.ebing.dog.api.bs.eps.forecastUnit;
-
-import java.util.List;
-
-public interface A0912Y610Bs {
-	void batchInsertForecastUnits(List<List<String>> sheetResults);
-}
--- src/main/java/cn/ebing/dog/api/service/eps/forecastUnit/A0912Y610Service.java
+++
@@ -1,5 +0,0 @@
-package cn.ebing.dog.api.service.eps.forecastUnit;
-
-public interface A0912Y610Service {
-	public void batchUploadForecastUnits(String name);
-}
