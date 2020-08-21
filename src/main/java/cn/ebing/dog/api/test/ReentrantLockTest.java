99a8401a27c7ecbf5f51ce5c72a87e26529ada02
hicery <qiuhengbin@codemao.cn>
2020-08-21 10:58:48 +0800

    1

--- src/main/java/cn/ebing/dog/api/test/ReentrantLockTest.java
+++ src/main/java/cn/ebing/dog/api/test/ReentrantLockTest.java
@@ -8,22 +8,25 @@ import java.util.concurrent.locks.ReentrantLock;
  * 独占锁，按顺序执行
  */
 public class ReentrantLockTest {
-	private static final Lock lock = new ReentrantLock(true);
+	private static final Lock lock = new ReentrantLock(false);
 	public static void main(String[] args) {
 		new Thread(() -> test(),"线程A").start();
 		new Thread(() -> test(),"线程B").start();
 		new Thread(() -> test(),"线程C").start();
+		new Thread(() -> test(),"线程1").start();
+		new Thread(() -> test(),"线程2").start();
+		new Thread(() -> test(),"线程3").start();
 	}
 	public static void  test()  {
 		for(int i=0;i<2;i++) {
 			try {
 				lock.lock();
 				System.out.println(Thread.currentThread().getName()+"获取了锁");
-				TimeUnit.SECONDS.sleep(1);
+				TimeUnit.SECONDS.sleep(0);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}finally {
-				System.out.println(Thread.currentThread().getName()+"释放了锁");
+//				System.out.println(Thread.currentThread().getName()+"释放了锁");
 				lock.unlock();
 			}
 		}
