6381e06e91bac8384a0f2004ea90f5d7adf4a2da
hicery <qiuhengbin@codemao.cn>
2020-08-24 10:44:17 +0800

    大堆 和 小堆

--- src/main/java/cn/ebing/dog/api/utils/sort/HeapSort1.java
+++ src/main/java/cn/ebing/dog/api/utils/sort/HeapSort1.java
@@ -10,13 +10,13 @@ public class HeapSort1 {
 		for (int i = length - 1; i > 0; i--) {
 			swap(array, 0, i);
 			length--;
-			sortCore(array, 0, length);
+			sortCoreMax(array, 0, length);
 		}
 	}
 
 	private static void buildMaxHeap(int[] array, int length) {
 		for (int i = length / 2; i >= 0; i--) {
-			sortCore(array, i, length);
+			sortCoreMax(array, i, length);
 		}
 	}
 
@@ -26,7 +26,7 @@ public class HeapSort1 {
 		array[j] =  temp;
 	}
 
-	private static void sortCore(int[] array, int i, int length) {
+	private static void sortCoreMax(int[] array, int i, int length) {
 		int left = i * 2 + 1;
 		int right = i * 2 + 2;
 		int largest = i;
@@ -38,15 +38,47 @@ public class HeapSort1 {
 		}
 		if (largest != i) {
 			swap(array, i, largest);
-			sortCore(array, largest, length);
+			sortCoreMax(array, largest, length);
 		}
 	}
 
 	public static void main(String[] args) {
 		int[] array = {9,2,4,6,1,5,7};
 		int length = array.length;
-		heapSort(array, length);
+		heapSortMin(array, length);
 		System.out.println("堆排序结果: " + Arrays.toString(array));
+	}
+
+	private static void heapSortMin(int[] array, int length) {
+		buildMinHeap(array, length);
+
+		for (int i = length - 1; i > 0; i--) {
+			swap(array, 0, i);
+			length--;
+			sortCoreMin(array, 0, length);
+		}
+	}
+
+	private static void buildMinHeap(int[] array, int length) {
+		for (int i = length / 2; i >= 0; i--) {
+			sortCoreMin(array, i, length);
+		}
+	}
+
+	private static void sortCoreMin(int[] array, int i, int length) {
+		int left = i * 2 + 1;
+		int right = i * 2 + 2;
+		int largest = i;
+		if (left < length && array[left] < array[largest]) {
+			largest = left;
+		}
+		if (right < length && array[right] < array[largest]) {
+			largest = right;
+		}
+		if (largest != i) {
+			swap(array, i, largest);
+			sortCoreMin(array, largest, length);
+		}
 	}
 
 }
