e3fc67972801a8d06e2ccd186129f0b423b1b10d
hicery <qiuhengbin@codemao.cn>
2020-12-04 09:00:33 +0800

    1

---
+++ src/main/java/cn/ebing/dog/api/utils/MD5Util.java
@@ -0,0 +1,128 @@
+package cn.ebing.dog.api.utils;
+
+import sun.misc.BASE64Encoder;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Util {
+    public static String getMD5(String text) {
+        String s = null;
+        byte[] source = text.getBytes();
+        char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };// 用来将字节转换成16进制表示的字符
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            md.update(source);
+            byte tmp[] = md.digest();// MD5 的计算结果是一个 128 位的长整数，
+            // 用字节表示就是 16 个字节
+            char str[] = new char[16 * 2];// 每个字节用 16 进制表示的话，使用两个字符， 所以表示成 16
+            // 进制需要 32 个字符
+            int k = 0;// 表示转换结果中对应的字符位置
+            for (int i = 0; i < 16; i++) {// 从第一个字节开始，对 MD5 的每一个字节// 转换成 16
+                // 进制字符的转换
+                byte byte0 = tmp[i];// 取第 i 个字节
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];// 取字节中高 4 位的数字转换,// >>>
+                // 为逻辑右移，将符号位一起右移
+                str[k++] = hexDigits[byte0 & 0xf];// 取字节中低 4 位的数字转换
+
+            }
+            s = new String(str);// 换后的结果转换为字符串
+
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return s;
+    }
+
+    /**
+     * 将数据进行 MD5 加密，并以16进制字符串格式输出
+     * @param data
+     * @return
+     */
+    public static String md5(String data) {
+        try {
+            MessageDigest md5=MessageDigest.getInstance("MD5");
+            BASE64Encoder base64en = new BASE64Encoder();
+            //加密后的字符串
+            return base64en.encode(md5.digest(data.getBytes("utf-8")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+
+    public static String getMD5(String plainText, int length) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");//获取MD5实例
+            md.update(plainText.getBytes());//此处传入要加密的byte类型值
+            byte[] digest = md.digest();//此处得到的是md5加密后的byte类型值
+            /*
+               下边的运算就是自己添加的一些二次小加密，记住这个千万不能弄错乱，
+                   否则在解密的时候，你会发现值不对的（举例：在注册的时候加密方式是一种，
+                在我们登录的时候是不是还需要加密它的密码然后和数据库的进行比对，但是
+            最后我们发现，明明密码对啊，就是打不到预期效果，这时候你就要想一下，你是否
+             有改动前后的加密方式）
+            */
+            int i;
+            StringBuilder sb = new StringBuilder();
+            for (int offset = 0; offset < digest.length; offset++) {
+                i = digest[offset];
+                if (i < 0) {
+                    i += 256;
+                }
+                if (i < 16) {
+                    sb.append(0);
+                }
+                sb.append(Integer.toHexString(i));//通过Integer.toHexString方法把值变为16进制
+            }
+            return sb.toString().substring(0, length);//从下标0开始，length目的是截取多少长度的值
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    private static final String KEY_MD5 = "MD5";
+    // 全局数组
+    private static final String[] strDigits = { "0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+
+    // 返回形式为数字跟字符串
+    private static String byteToArrayString(byte bByte) {
+        int iRet = bByte;
+        if (iRet < 0) {
+            iRet += 256;
+        }
+        int iD1 = iRet / 16;
+        int iD2 = iRet % 16;
+        return strDigits[iD1] + strDigits[iD2];
+    }
+
+    // 转换字节数组为16进制字串
+    private static String byteToString(byte[] bByte) {
+        StringBuffer sBuffer = new StringBuffer();
+        for (int i = 0; i < bByte.length; i++) {
+            sBuffer.append(byteToArrayString(bByte[i]));
+        }
+        return sBuffer.toString();
+    }
+    /**
+     * MD5加密
+     * @param strObj
+     * @return
+     * @throws Exception
+     */
+    public static String getMD5Code(String strObj) {
+        try {
+            MessageDigest md = MessageDigest.getInstance(KEY_MD5);
+            // md.digest() 该函数返回值为存放哈希值结果的byte数组
+            return byteToString(md.digest(strObj.getBytes()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+}
