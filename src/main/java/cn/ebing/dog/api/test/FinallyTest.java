4e4da2af8b4e1723dfef6f7dda54ce63f7dbd136
hicery <qiuhengbin@codemao.cn>
2021-02-23 15:03:37 +0800

    1

--- src/main/java/cn/ebing/dog/api/test/FinallyTest.java
+++ src/main/java/cn/ebing/dog/api/test/FinallyTest.java
@@ -1,5 +1,8 @@
 package cn.ebing.dog.api.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 如果 try 中有 return 语句， 那么 finally 中的代码还是会执⾏。因为 return 表⽰的是 要整个⽅法体返回， 所以，finally 中的语句会在 return 之前执⾏。 但是 return 前执行的 finally 块内，对数据的修改效果对于引用类型和值类型会不同：
  */
@@ -7,11 +10,11 @@ public class FinallyTest {
     public static void main(String[] args) {
         //System.out.println("finally1()-> " + finally1());
         //System.out.println("finally2()-> " + finally2());
-        int[] f3 = finally3();
-        for (int i : f3) {
-            System.out.println("finally3-> " + i + "; ints.length ->" + f3.length);
-
-        }
+//        int[] f3 = finally3();
+//        for (int i : f3) {
+//            System.out.println("finally3-> " + i + "; ints.length ->" + f3.length);
+//        }
+        arrayList();
     }
 
     static int finally1() {
@@ -34,6 +37,16 @@ public class FinallyTest {
             System.out.println("finally2");
         }
     }
+
+    static void arrayList() {
+        List<Integer> list = new ArrayList<>();
+        list.add(null);
+        list.add(1);
+        for (Integer i: list) {
+            System.out.println("i-> " + i);
+        }
+    }
+
     static int[] finally3() {
         int[] ints = new int[]{0};
         System.out.println("ints.length ->" + ints.length + "; ints[0] ->" + ints[0]);
