d879475c2271e95ff77f6b15ee03f48e9ecba142
hicery <qiuhengbin@codemao.cn>
2021-07-23 15:15:40 +0800

    code

--- src/main/java/cn/ebing/dog/api/service/impl/UserServiceImpl.java
+++ src/main/java/cn/ebing/dog/api/service/impl/UserServiceImpl.java
@@ -1,109 +1,109 @@
-//package cn.ebing.dog.api.service.impl;
-//
-//import cn.ebing.dog.api.domain.business.Hero;
-//import cn.ebing.dog.api.domain.entity.UserEntity;
-//import cn.ebing.dog.api.domain.query.UserQuery;
-//import cn.ebing.dog.api.domain.request.UserRequest;
-//import cn.ebing.dog.api.domain.response.UserResponse;
-//import cn.ebing.dog.api.mapper.UserMapper;
-//import cn.ebing.dog.api.service.UserService;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.time.LocalDateTime;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.function.Predicate;
-//
-//@Service
-//public class UserServiceImpl implements UserService {
-//
-//	private static Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
-//
-//	@Autowired
-//	private UserMapper userMapper;
-//
-//	@Autowired
-//	private UserService userService;
-//
-//	@Override
-//	public UserResponse getUserById(Integer id) {
-//		logger.debug("开始进行 getUserById");
-//		UserEntity user = userMapper.getById(id);
-//		if (user == null) {
-//			throw new RuntimeException("USER 不存在----");
-//		}
-//		logger.debug("找到了数据，准备返回");
-//		return null;
-//	}
-//
-//	@Override
-//	public List<UserEntity> listUsers() {
-//		LocalDateTime minCreateTime = LocalDateTime.now();
-//		UserQuery query = new UserQuery();
-//		query.setMinCreateTime(minCreateTime);
-//		List<UserEntity> users = userMapper.listByQuery(query);
-//		return users;
-//	}
-//
-//	@Override
-//	public int saveUser(UserRequest request) {
-//		userService.saveUser2(request);
-//		return 1;
-//	}
-//
-//	@Override
-//	@Transactional
-//	public void saveUser2(UserRequest request) {
-//		UserEntity user = new UserEntity(request.getName(), request.getAge(), request.getSex());
-//		userMapper.addOne(user);
-//		throw new Error();
-//	}
-//
-//	@Override
-//	public int updateUser(Integer id, UserRequest request) {
-////		UserEntity user = new UserEntity(id, request.getName(), request.getAge(), request.getSex());
-//		return 1;
-//	}
-//
-//	@Override
-//	public void deleteUserById(Integer id) {
-//		userMapper.deleteById(id);
-//	}
-//
-//	/**
-//	 * 策略模式
-//	 * @param heroList
-//	 * @param predicate
-//	 * @return
-//	 */
-//	public static List<Hero> getHero(List<Hero> heroList, Predicate<Hero> predicate){
-//		List<Hero> result = new ArrayList<Hero>();
-//		for(Hero hero : heroList){
-//			if(predicate.test(hero)){
-//				result.add(hero);
-//			}
-//		}
-//		return result;
-//	}
-//
-//	@Override
-//	public void heroTest() {
-//
-//		/**
-//		 * 当lambda用于取代匿名内部类时，它与匿名内部类的本质区别是什么呢
-//		 * 答：简化缩写而已
-//		 */
-//		getHero(new ArrayList<Hero>(), new Predicate<Hero>() {
-//			@Override
-//			public boolean test(Hero t) {
-//				return t.getStature() > 170 && "刺客".equals(t.getType());
-//			}
-//		});
-//
-//		getHero(new ArrayList<Hero>(), (t)->t.getStature() > 170 && "刺客".equals(t.getType()));
-//	}
-//}
+package cn.ebing.dog.api.service.impl;
+
+import cn.ebing.dog.api.domain.business.Hero;
+import cn.ebing.dog.api.domain.entity.UserEntity;
+import cn.ebing.dog.api.domain.query.UserQuery;
+import cn.ebing.dog.api.domain.request.UserRequest;
+import cn.ebing.dog.api.domain.response.UserResponse;
+import cn.ebing.dog.api.mapper.UserMapper;
+import cn.ebing.dog.api.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
+	@Autowired
+	private UserMapper userMapper;
+
+	@Autowired
+	private UserService userService;
+
+	@Override
+	public UserResponse getUserById(Integer id) {
+		logger.debug("开始进行 getUserById");
+		UserEntity user = userMapper.getById(id);
+		if (user == null) {
+			throw new RuntimeException("USER 不存在----");
+		}
+		logger.debug("找到了数据，准备返回");
+		return null;
+	}
+
+	@Override
+	public List<UserEntity> listUsers() {
+		LocalDateTime minCreateTime = LocalDateTime.now();
+		UserQuery query = new UserQuery();
+		query.setMinCreateTime(minCreateTime);
+		List<UserEntity> users = userMapper.listByQuery(query);
+		return users;
+	}
+
+	@Override
+	public int saveUser(UserRequest request) {
+		userService.saveUser2(request);
+		return 1;
+	}
+
+	@Override
+	@Transactional
+	public void saveUser2(UserRequest request) {
+		UserEntity user = new UserEntity(request.getName(), request.getAge(), request.getSex());
+		userMapper.addOne(user);
+		throw new Error();
+	}
+
+	@Override
+	public int updateUser(Integer id, UserRequest request) {
+//		UserEntity user = new UserEntity(id, request.getName(), request.getAge(), request.getSex());
+		return 1;
+	}
+
+	@Override
+	public void deleteUserById(Integer id) {
+		userMapper.deleteById(id);
+	}
+
+	/**
+	 * 策略模式
+	 * @param heroList
+	 * @param predicate
+	 * @return
+	 */
+	public static List<Hero> getHero(List<Hero> heroList, Predicate<Hero> predicate){
+		List<Hero> result = new ArrayList<Hero>();
+		for(Hero hero : heroList){
+			if(predicate.test(hero)){
+				result.add(hero);
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public void heroTest() {
+
+		/**
+		 * 当lambda用于取代匿名内部类时，它与匿名内部类的本质区别是什么呢
+		 * 答：简化缩写而已
+		 */
+		getHero(new ArrayList<Hero>(), new Predicate<Hero>() {
+			@Override
+			public boolean test(Hero t) {
+				return t.getStature() > 170 && "刺客".equals(t.getType());
+			}
+		});
+
+		getHero(new ArrayList<Hero>(), (t)->t.getStature() > 170 && "刺客".equals(t.getType()));
+	}
+}
--- src/main/java/cn/ebing/dog/api/thread/DeadLockSample.java
+++ src/main/java/cn/ebing/dog/api/thread/DeadLockSample.java
@@ -1,79 +1,79 @@
-//package cn.ebing.dog.api.thread;
-//
-//
-//import java.lang.management.ManagementFactory;
-//import java.lang.management.ThreadInfo;
-//import java.lang.management.ThreadMXBean;
-//import java.util.concurrent.Executors;
-//import java.util.concurrent.ScheduledExecutorService;
-//import java.util.concurrent.TimeUnit;
-//
-//public class DeadLockSample extends Thread {
-//	private String first;
-//	private String second;
-//	public DeadLockSample(String name, String first, String second) {
-//		super(name);
-//		this.first = first;
-//		this.second = second;
-//	}
-//
-//	@Override
-//	public  void run() {
-//		synchronized (first) {
-//			System.out.println(this.getName() + " obtained: " + first);
-//			try {
-//				Thread.sleep(1000L);
-//				synchronized (second) {
-//					System.out.println(this.getName() + " obtained: " + second);
-//				}
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//	}
-//
-//	/**
-//	 * 1 jps
-//	 * 2 jstack [pid]
-//	 *
-//	 * 如果知道死锁。1、用上面的Java 命令。2、用 ThreadMXBean 的 Java代码扫描。然后每10秒进行一次死锁扫描
-//	 */
-//	public static void main(String[] args) throws InterruptedException {
-//		ThreadMXBean mbean = ManagementFactory.getThreadMXBean();
-//		Runnable dlCheck = new Runnable() {
-//			@Override
-//			public void run() {
-//				long[] threadIds = mbean.findDeadlockedThreads();
-//				if (threadIds != null) {
-//					ThreadInfo[] threadInfos = mbean.getThreadInfo(threadIds);
-//					System.out.println("Detected deadlock threads:");
-//					for (ThreadInfo threadInfo : threadInfos) {
-//						System.out.println(threadInfo.getThreadName());
-//					}
-//				}
-//			}
-//		};
-//
-//		ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-//		// 稍等5秒，然后每10秒进行一次死锁扫描
-//		scheduler.scheduleAtFixedRate(dlCheck, 5L, 10L, TimeUnit.SECONDS);
-//		/**
-//		 * 下面是死锁的 demo 代码
-//		 */
-//		String lockA = "lockA";
-//		String lockB = "lockB";
-//		DeadLockSample t1 = new DeadLockSample("Thread1", lockA, lockB);
-//		DeadLockSample t2 = new DeadLockSample("Thread2", lockB, lockA);
-//		/**
-//		 * 这就是因为线程调度依赖于（操作系统）调度器，虽然你可以通过优先级之类进行影响，但是具体情况是不确定的。
-//		 */
-//		t1.start();
-//		t2.start();
-//    /**
-//     * https://www.jianshu.com/p/ff637be3b671 线程的合并的含义就是 将几个并行线程的线程合并为一个单线程执行，应用场景是
-//     * 当一个线程必须等待另一个线程执行完毕才能执行时，Thread类提供了join方法来完成这个功能，注意，它不是静态方法。
-//     */
-//    t1.join();
-//		t2.join();
-//	}
-//}
+package cn.ebing.dog.api.thread;
+
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class DeadLockSample extends Thread {
+	private String first;
+	private String second;
+	public DeadLockSample(String name, String first, String second) {
+		super(name);
+		this.first = first;
+		this.second = second;
+	}
+
+	@Override
+	public  void run() {
+		synchronized (first) {
+			System.out.println(this.getName() + " obtained: " + first);
+			try {
+				Thread.sleep(1000L);
+				synchronized (second) {
+					System.out.println(this.getName() + " obtained: " + second);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * 1 jps
+	 * 2 jstack [pid]
+	 *
+	 * 如果知道死锁。1、用上面的Java 命令。2、用 ThreadMXBean 的 Java代码扫描。然后每10秒进行一次死锁扫描
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		ThreadMXBean mbean = ManagementFactory.getThreadMXBean();
+		Runnable dlCheck = new Runnable() {
+			@Override
+			public void run() {
+				long[] threadIds = mbean.findDeadlockedThreads();
+				if (threadIds != null) {
+					ThreadInfo[] threadInfos = mbean.getThreadInfo(threadIds);
+					System.out.println("Detected deadlock threads:");
+					for (ThreadInfo threadInfo : threadInfos) {
+						System.out.println(threadInfo.getThreadName());
+					}
+				}
+			}
+		};
+
+		ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+		// 稍等5秒，然后每10秒进行一次死锁扫描
+		scheduler.scheduleAtFixedRate(dlCheck, 5L, 10L, TimeUnit.SECONDS);
+		/**
+		 * 下面是死锁的 demo 代码
+		 */
+		String lockA = "lockA";
+		String lockB = "lockB";
+		DeadLockSample t1 = new DeadLockSample("Thread1", lockA, lockB);
+		DeadLockSample t2 = new DeadLockSample("Thread2", lockB, lockA);
+		/**
+		 * 这就是因为线程调度依赖于（操作系统）调度器，虽然你可以通过优先级之类进行影响，但是具体情况是不确定的。
+		 */
+		t1.start();
+		t2.start();
+    /**
+     * https://www.jianshu.com/p/ff637be3b671 线程的合并的含义就是 将几个并行线程的线程合并为一个单线程执行，应用场景是
+     * 当一个线程必须等待另一个线程执行完毕才能执行时，Thread类提供了join方法来完成这个功能，注意，它不是静态方法。
+     */
+    t1.join();
+		t2.join();
+	}
+}
